96d030e8506c3915741918ae0cf9dae55e86aca1
Ujjwal Singh <ujzzzwall@gmail.com>
2026-03-09 11:51:28 +0530

    first commit Added basic structure

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      
+      <Routes>
+        {/* public route */}
+        <Route element={<AuthLayout/>}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private route */}
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home />} />
+        </Route>
+
+      </Routes>
+
+    </main>
+  );
+};
+
+export default App;
